273358218d4f42f36746993e4fe1685547ad213e
Harpreet Singh <hurpreet@icloud.com>
2023-08-30 17:17:25 -0400

    Added 2nd table named Item

--- models.py
+++ models.py
@@ -1,24 +1,45 @@
-from sqlalchemy import Column, Integer, String, Text
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship, backref
 
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
     id= Column(Integer, primary_key=True)
-    firstname = Column(String, nullable=False)
-    lastname = Column(String, nullable=False)
-    contact = Column(Text, nullable=False)
+    name = Column(String, nullable=False)
+    email = Column(String(55), nullable=False)
+
+    item= relationship("Item", backref= "user")
 
     def __repr__(self):
         return (
             f"<User: id= {self.id}, \n"
-            f"firstname= '{self.firstname}', \n"
-            f"lastname= '{self.lastname}, \n"
-            f"contact= '{self.contact}>\n\n"
+            f"name= '{self.name}', \n"
+            f"email= '{self.email}>\n\n"
         )
 
+class Item(Base):
+    __tablename__ = 'items'
+
+    id= Column(Integer, primary_key=True)
+    item_name= Column(String, nullable=False)
+    status= Column(String(10))
+    found_by= Column(String)
+    claimed_by= Column(String)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    def __repr__(self):
+        return f"\n<Item " \
+            + f"id={self.id}, " \
+            + f"item_name={self.item_name}, " \
+            + f"status={self.status}, " \
+            + f"found_by={self.found_by}, " \
+            + f"claimed_by={self.claimed_by}, " \
+            + f"color={self.color}, " \
+            + f"weight={self.weight}" \
+            + ">"
 
     
 
--- seeds.py
+++ seeds.py
@@ -1,9 +1,23 @@
 from models import User 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import ipdb
+from faker import Faker
+
+fake = Faker()
+
+ipdb.set_trace()
 
 
 engine = create_engine("sqlite:///main.db")
 
 Session= sessionmaker(bind=engine)
 session= Session()
+
+
+
+# #Use faker to seed data for user table
+# for _ in range(10):
+
+#     user= User(firstname= fake.first_name(), lastname= fake.last_name(), email= fake.ascii_company_email())
+#     print(user)
