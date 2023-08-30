8fd9aaf43b47d850120c72e14de8a60b3df5c376
Harpreet Singh <hurpreet@icloud.com>
2023-08-30 00:14:16 -0400

    Created and configured seeds.py

---
+++ seeds.py
@@ -0,0 +1,9 @@
+from models import User 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine("sqlite:///main.db")
+
+Session= sessionmaker(bind=engine)
+session= Session()
