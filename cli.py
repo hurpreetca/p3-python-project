19181c6bda393c8f25eb30740518dea9b8a8ec71
Harpreet Singh <hurpreet@icloud.com>
2023-08-31 00:43:08 -0400

    Added prettycli and simple-term-menu packages

---
+++ cli.py
@@ -0,0 +1,11 @@
+class cli():
+    def __init__(self):
+        pass
+
+    def start():
+
+        print("Step into the gateway of rediscovery at our Lost and Found Box! \n")
+        print("1. Lost an Item")
+        print("2. Found an Item")
+        print("3. ")
+        print()
