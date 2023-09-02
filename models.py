58987a1db4ec360935235e7f6d098cbc0dad629b
Harpreet Singh <hurpreet@icloud.com>
2023-09-01 23:31:34 -0400

    Fixed Bug with the tables

--- models.py
+++ models.py
@@ -3,14 +3,15 @@ from sqlalchemy.orm import declarative_base, relationship, backref
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'users'
 
-    id= Column(Integer, primary_key=True)
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     email = Column(String(55), nullable=False)
 
-    item= relationship("Item", backref= "user")
+    item = relationship("Item", backref="users")
 
     def __repr__(self):
         return (
@@ -19,24 +20,23 @@ class User(Base):
             f"email= '{self.email}>\n\n"
         )
 
-class Item(Base):
-    __tablename__ = 'items'
 
-    id= Column(Integer, primary_key=True)
-    item_name= Column(String, nullable=False)
-    status= Column(String(10))
-    found_by= Column(String)
-    claimed_by= Column(String)
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True)
+    item_name = Column(String, nullable=False)
+    status = Column(String(10))
+    resolution_status = Column(String)
 
     user_id = Column(Integer, ForeignKey("users.id"))
 
     def __repr__(self):
-        return f"\n<Item " \
-            + f"id={self.id}, " \
-            + f"item_name={self.item_name}, " \
-            + f"status={self.status}, " \
-            + f"found_by={self.found_by}, " \
-            + f"claimed_by={self.claimed_by}, " \
+        return (
+            f"\n<Item "
+            + f"id={self.id}, "
+            + f"item_name={self.item_name}, "
+            + f"item_status={self.item_status}, "
+            + f"resolution_status={self.resolution_status}, "
             + ">"
-    
-
+        )
